a338d48875907e1c1a3da9274b8d510422019baf
Chir <chirpatelss@gmail.com>
2021-05-02 20:22:35 +0530

    .

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 
 connectDB();
 app.use(express.json({ extend: false }));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.get("/", function(req, res, next) {
   res.json({ message: "alive" });
 });
@@ -19,9 +19,9 @@ app.use("/api/crypto", require("./routes/cryptoAPI"));
 app.use("/data/crypto", require("./routes/cryptoDatabase"));
 
 
-app.set('port', process.env.PORT || 3001);
-console.log("Express server listening on port " + app.get('port'));
-app.listen(app.get('port'));
+// app.set('port', process.env.PORT || 3001);
+// console.log("Express server listening on port " + app.get('port'));
+// app.listen(app.get('port'));
 
 
 module.exports = app;
